6eda736e84af42f4bac520123ed2ab0cbc1a9f86
Narciso Lobo <44825108+narcisolobo@users.noreply.github.com>
2023-03-08 09:39:47 -0800

    w1d3 morning notes and code

---
+++ W1D3/loops.js
@@ -0,0 +1,37 @@
+// your boolean expression means 'as long as'
+
+for (var i = 1; i <= 20; i++) {
+  console.log(i);
+}
+
+// after loop
+
+// increment operator
+// these two lines are equivalent
+i = i + 1
+i++
+
+// a loop that counts down
+for (var i = 2; i >= -3; i--) {
+  console.log(i);
+}
+
+// a loop that prints the odd numbers from
+// 1 to 10
+for (var i = 1; i <= 10; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+for (var i = 1; i<= 10; i += 2) {
+  console.log(i);
+}
+
+// shorthand += operator
+i = i + 2
+i += 2
+
+// Modulo Operator - %
+// Division Remainder Operator
+
